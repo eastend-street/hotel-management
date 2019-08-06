ac8d1efe2664293231443998fc4a236088bb5a0e
eastend-street <ss_1322irf@icloud.com>
2019-08-06 13:59:05 -0700

    check hello world

---
+++ src/Hotel.java
@@ -0,0 +1,5 @@
+public class Hotel {
+    public Hotel(){
+        System.out.println("hello");
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,5 @@
+public class main {
+    public static void main(String[] args) {
+        Hotel hotel = new Hotel();
+    }
+}
